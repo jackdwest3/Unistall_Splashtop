014df132a3905dca4442c96584ff9ba00237123c
jackdwest3 <143582669+jackdwest3@users.noreply.github.com>
2023-09-07 11:35:14 -0500

    Create Uninstall_Splashtop.ps1

---
+++ Uninstall_Splashtop.ps1
@@ -0,0 +1,25 @@
+# Uninstall Splashtop software and remove associated registry entries and folders
+
+# Uninstall the Splashtop software using MSIExec
+$msiExecArgs = "/x {B7C5EA94-B96A-41F5-BE95-25D78B486678} /qn"
+Start-Process -FilePath "msiexec.exe" -ArgumentList $msiExecArgs -Wait
+
+# Delete registry entries for Splashtop Inc. (32-bit and 64-bit)
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Splashtop Inc." -Name "*" -Force -ErrorAction SilentlyContinue
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Splashtop Inc." -Name "*" -Force -ErrorAction SilentlyContinue
+
+# Remove the Splashtop Server folder
+Remove-Item -Path "C:\Program Files (x86)\Splashtop\Splashtop Remote\Server" -Recurse -Force -ErrorAction SilentlyContinue
+
+# Remove the Splashtop Temp folder
+Remove-Item -Path "C:\ProgramData\Splashtop\Temp" -Recurse -Force -ErrorAction SilentlyContinue
+
+# Stop and delete the SSUService
+Stop-Service -Name "SSUService" -ErrorAction SilentlyContinue
+Remove-Service -Name "SSUService" -ErrorAction SilentlyContinue
+
+# Remove the Splashtop Software Updater folders
+Remove-Item -Path "C:\ProgramData\Splashtop\Splashtop Software Updater" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Program Files (x86)\Splashtop\Splashtop Software Updater" -Recurse -Force -ErrorAction SilentlyContinue
+
+Write-Host "Splashtop software and associated entries have been removed."
